7ddaed0ddaeafa9f71cd0c30a1539a76d2a02d02
cjohnson <collin.g.johnson@gmail.com>
2024-05-26 17:09:41 -0400

    Finish part 6, add search

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ASPNetCoreRazorPagesTutorial.Data;
 using ASPNetCoreRazorPagesTutorial.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ASPNetCoreRazorPagesTutorial.Pages.Movies
 {
@@ -21,9 +22,34 @@ namespace ASPNetCoreRazorPagesTutorial.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
