4a80db13d03cfc856fcc088af7ef9afd3f3181ca
cjohnson <collin.g.johnson@gmail.com>
2024-05-25 22:43:31 -0400

    Finish Part 5, update the generated pages in ASP.NET Core app

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPNetCoreRazorPagesTutorial.Models;
 
@@ -6,8 +7,10 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
