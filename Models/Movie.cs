f6b9051f2029d56b0036a8106889e8d3a9959c0f
cjohnson <collin.g.johnson@gmail.com>
2024-05-26 21:30:33 -0400

    Finish tutorial

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,12 +6,20 @@ namespace ASPNetCoreRazorPagesTutorial.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+
+    [StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; } = string.Empty;
+
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [Range(1, 100), DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
     public string Rating { get; set; } = string.Empty;
 }
